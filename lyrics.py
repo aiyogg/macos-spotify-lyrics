32f9fa3ba4a0c55201ff1832de84e0e5f25455a0
chuck <tengchenc@gmail.com>
2023-10-20 01:02:16 +0800

    feat: modify the lyrics to SGAPI

--- lyrics.py
+++ lyrics.py
@@ -1,92 +1,90 @@
-from subprocess import Popen, PIPE
 import json
+import os
+from subprocess import PIPE, Popen
 
 import requests
-import bs4
+from dotenv import load_dotenv
+from zhconv import convert
+
+load_dotenv()
 
 config = {
-	'genius_token': "",
-	'genius_base_url': 'https://api.genius.com'
+    "sg_key": os.getenv("SG_KEY"),
+    "sg_base_url": os.getenv("SG_BASE_URL"),
 }
 
+base_params = {"key": str(config["sg_key"]), "type": "kg", "format": 1}
+
+
 def exec_applescript(script):
-	p = Popen(['osascript', '-e', script], stdout=PIPE).communicate()
-	return json.loads(p[0])
+    p = Popen(["osascript", "-e", script], stdout=PIPE).communicate()
+    return json.loads(p[0])
+
 
 def get_spotify_now_playing():
-	spotify_now_playing_applescript = '''
+    spotify_now_playing_applescript = """
 tell application "System Events"
-	set processList to (name of every process)
+  set processList to (name of every process)
 end tell
 if (processList contains "Spotify") is true then
-	tell application "Spotify"
-		set artistName to artist of current track
-		set trackName to name of current track
-		set albumName to album of current track
-		return "{" & "\\\"artist\\\": \\\"" & artistName & "\\\", " & "\\\"track\\\": \\\"" & trackName & "\\\", \\\"album\\\": \\\"" & albumName & "\\\"}"
-	end tell
-end if'''
+  tell application "Spotify"
+    set artistName to artist of current track
+    set trackName to name of current track
+    set albumName to album of current track
+    return "{" & "\\\"artist\\\": \\\"" & artistName & "\\\", " & "\\\"track\\\": \\\"" & trackName & "\\\", \\\"album\\\": \\\"" & albumName & "\\\"}"
+  end tell
+end if"""
 
-	return exec_applescript(spotify_now_playing_applescript)
+    return exec_applescript(spotify_now_playing_applescript)
 
-def request_genius_song_info(song_title, artist_name):
-	genius_base_url = config['genius_base_url']
-	headers = {'Authorization': 'Bearer ' + config['genius_token']}
-	search_url = genius_base_url + '/search'
-	data = {'q': song_title + ' ' + artist_name}
-	response = requests.get(search_url, data=data, headers=headers)
-	return response
 
-def extract_genius_lyrics_angular(html):
-	lyrics = html.find('div', class_='lyrics').get_text()
-	return lyrics
+def request_sg_song_info(song_title, artist_name):
+    sg_base_url = config["sg_base_url"]
+    sg_key = config["sg_key"]
 
-def extract_genius_lyrics_react(html):
-	lyric_elements = html.select('div[class*="Lyrics__Container"]')[0]
-	lyrics = ""
+    search_url = sg_base_url + "/api/music/search"
+    data = dict(
+        list(base_params.items())
+        + [("name", song_title), ("type", "kg"), ("page", 1), ("limit", 10)]
+    )
 
-	for elem in lyric_elements:
-		if isinstance(elem, bs4.NavigableString):
-			lyrics += elem
-		elif elem.name == "br":
-			lyrics += "\n"
-		else:
-			lyrics += elem.decode_contents().replace("<br/>", "\n")
+    response = requests.get(search_url, params=data)
+    return response.json()
 
-	return lyrics
 
-def scrape_genius_song_url(url):
-	page = requests.get(url)
-	html = bs4.BeautifulSoup(page.text, 'html.parser')
+def request_sg_song_lyrics(song_id):
+    genius_base_url = config["sg_base_url"]
+    lyrics_url = genius_base_url + "/api/music/lrc"
+    data = dict(list(base_params.items()) + [("id", song_id)])
+    response = requests.get(lyrics_url, params=data)
+    return response.json()
 
-	if html.find('div', class_='lyrics') is not None:
-		return extract_genius_lyrics_angular(html)
-	else:
-		return extract_genius_lyrics_react(html)
 
-def get_lyrics_from_genius(song_title, artist_name):
-	song_info = request_genius_song_info(song_title, artist_name).json()
-	remote_song_info = None
+def get_lyrics_from_sg(song_title, artist_name):
+    song_info = request_sg_song_info(song_title, artist_name)
+    remote_song_info = None
 
-	for hit in song_info['response']['hits']:
-		if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
-			remote_song_info = hit
-			break
+    # print("song_info: " + json.dumps(song_info, indent=2, ensure_ascii=False))
 
-	# Extract lyrics from URL if song was found
-	if remote_song_info:
-		song_url = remote_song_info['result']['url']
-		lyrics = scrape_genius_song_url(song_url).strip("\n")
-		return lyrics
+    for hit in song_info["data"]:
+        if convert(artist_name.lower(), "zh-cn") in hit["artist"].lower():
+            remote_song_info = hit
+            break
+
+    if remote_song_info:
+        song_id = remote_song_info["id"]
+        lyrics = request_sg_song_lyrics(song_id)["data"].strip("\r\n")
+        return lyrics
+
 
 def print_spotify_now_playing_lyrics():
-	info = get_spotify_now_playing()
-	lyrics = get_lyrics_from_genius(info['track'], info['artist'])
-	print()
-	print(info['track'] + " by " + info['artist'] + " from album " + info["album"])
-	print()
-	print(lyrics)
-	print()
+    info = get_spotify_now_playing()
+    lyrics = get_lyrics_from_sg(info["track"], info["artist"])
+    print()
+    print(info["track"] + " by " + info["artist"] + " from album " + info["album"])
+    print()
+    print(lyrics)
+    print()
+
 
 print_spotify_now_playing_lyrics()
-
